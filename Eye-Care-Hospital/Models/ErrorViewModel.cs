ac8082c1fa8ce282521053601f9a08d9dc16e352
Sakshi Pawar <19sakshipawar@gmail.com>
2024-11-25 12:00:35 +0530

    Add project files.

---
+++ Eye-Care-Hospital/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Eye_Care_Hospital.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
